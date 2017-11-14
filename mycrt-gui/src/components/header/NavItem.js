cf292cae122d37131edf96b0142dcaee139e3f5d
Audrey Chan <audreyc@sonic.net>
2017-11-14 11:06:59 -0800

    override link styling

--- mycrt-gui/src/components/header/NavItem.js
+++ mycrt-gui/src/components/header/NavItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import './NavItem.css';
-import '../../bootstrap-3.3.7-dist/css/bootstrap.min.css'
+//import '../../bootstrap-3.3.7-dist/css/bootstrap.min.css'
 
 class NavItem extends Component {
   render() {
