b3ca74918d0f6faa84074024d8057d7ab84615b0
Audrey Chan <audreyc@sonic.net>
2017-11-15 01:17:18 -0800

    add header icons

--- mycrt-gui/src/components/header/Header.js
+++ mycrt-gui/src/components/header/Header.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import './Header.css';
+import '../../bootstrap-3.3.7-dist/css/bootstrap.min.css';
+
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-        header here, title
+        <div class="glyphicon glyphicon-question-sign" />
+        <div class="glyphicon glyphicon-cog" />
+        <div class="glyphicon glyphicon-envelope" />
       </div>
     );
   }
