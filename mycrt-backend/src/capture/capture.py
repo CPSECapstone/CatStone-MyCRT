21d92fe8da17c2fd3a4ca567c40c733521b4d018
Arjun <arjungup10@gmail.com>
2017-11-16 01:19:10 -0800

    Added basic functionality to download log file (hardcoded)

---
+++ mycrt-backend/src/capture/capture.py
@@ -0,0 +1,35 @@
+import argparse
+import os.path
+import sys
+import boto3
+from botocore.exceptions import NoRegionError, ClientError
+
+region = 'us-west-1'
+rds_instance = 'testdb'
+log_file = 'general/mysql-general.log'
+local_log_file = 'log'
+
+try:
+    rds = boto3.client('rds', region)
+except NoRegionError:
+    rds = boto3.client('rds','us-west-1')
+
+with open(local_log_file, 'w') as f:
+    print('downloading {rds} log file {file}'.format(rds=rds_instance, file=log_file))
+    token = '0'
+    try:
+        response = rds.download_db_log_file_portion(
+            DBInstanceIdentifier=rds_instance,
+            LogFileName=log_file,
+            Marker=token)
+        while response['AdditionalDataPending']:
+            f.write(response['LogFileData'])
+            token=response['Marker']
+            response = rds.download_db_log_file_portion(
+                DBInstanceIdentifier=rds_instance,
+                LogFileName=log_file,
+                Marker=token)
+        f.write(response['LogFileData'])
+    except ClientError as e:
+        print(e)
+        sys.exit(2)
