87f8ff25bf6585e5ce68a4f7c4a23cedaf8779df
Andrew Ly <andyrewly@gmail.com>
2018-01-14 19:31:25 -0800

    Created SQL Scripts for Table creation and deletion

---
+++ mycrt-db/CAT-DB-CLEAR.sql
@@ -0,0 +1,6 @@
+DROP TABLE Notifications;
+DROP TABLE Metrics;
+DROP TABLE Replays;
+DROP TABLE Captures;
+
+DROP TABLE Users;
---
+++ mycrt-db/CAT-DB-INIT.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Users(
+   userId           INT AUTO_INCREMENT,
+   userName         VARCHAR(64),
+   email            VARCHAR(64),
+   notificationLife INT,
+   PRIMARY KEY (userId)
+);
+
+CREATE TABLE Notifications(
+   notificationId      INT AUTO_INCREMENT,
+   userId              INT,
+   notificationMessage VARCHAR(64),
+   timeActive          INT,
+   PRIMARY KEY (notificationId),
+   FOREIGN KEY (userId) references Users(userId)
+);
+
+CREATE TABLE Captures(
+   captureId    INT AUTO_INCREMENT,
+   userId       INT,
+   captureAlias VARCHAR(64) UNIQUE,
+   startTime    DATETIME,
+   endTime      DATETIME,
+   s3Bucket     VARCHAR(64),
+   logFileName  VARCHAR(64),
+   rdsInstance  VARCHAR(64),
+   PRIMARY KEY (captureId),
+   FOREIGN KEY (userId) references Users(userId)
+);
+
+CREATE TABLE Replays(
+   replayId    INT AUTO_INCREMENT,
+   userId      INT,
+   captureId   INT,
+   replayAlias VARCHAR(64) UNIQUE,
+   rdsInstance VARCHAR(64),
+   PRIMARY KEY (replayId),
+   FOREIGN KEY (userId) references Users(userId),
+   FOREIGN KEY (captureId) references Captures(captureId)
+);
+
+CREATE TABLE Metrics(
+   metricId       INT AUTO_INCREMENT,
+   captureId      INT,
+   replayId       INT,
+   s3Bucket       VARCHAR(64),
+   metricFileName VARCHAR(64),
+   PRIMARY KEY (metricId),
+   FOREIGN KEY (captureId) references Captures(captureId),
+   FOREIGN KEY (replayId) references Replays(replayId)
+);
