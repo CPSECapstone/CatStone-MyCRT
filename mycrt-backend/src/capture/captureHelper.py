f7c78f1a7f2f255ced3363916b6de4c1e2f21cfe
Arjun <arjungup10@gmail.com>
2018-01-20 12:49:53 -0800

    Created file with capture helper functions

---
+++ mycrt-backend/src/capture/captureHelper.py
@@ -0,0 +1,10 @@
+import argparse
+import os.path
+import sys
+import boto3
+import json
+from botocore.exceptions import NoRegionError, ClientError
+
+#AWS setup
+s3 = boto3.client('s3')
+rds = boto3.client('rds')
