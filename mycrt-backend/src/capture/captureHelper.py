5a8afdcfeb3d6b8a0f3b11f27364d7d67a0692b7
Arjun <arjungup10@gmail.com>
2018-01-20 12:50:41 -0800

    Added basic functionality to get all RDS instances of a user

--- mycrt-backend/src/capture/captureHelper.py
+++ mycrt-backend/src/capture/captureHelper.py
@@ -8,3 +8,13 @@ from botocore.exceptions import NoRegionError, ClientError
 #AWS setup
 s3 = boto3.client('s3')
 rds = boto3.client('rds')
+
+def getRDSInstances():
+    DBInstances = []
+
+    response = rds.describe_db_instances()
+    DBInstancesInfo = response['DBInstances']
+    for instance in DBInstancesInfo:
+        DBInstances.append(instance['DBInstanceIdentifier'])
+
+    return DBInstances
