4ce78c6fb3b9b9b55496a22d5f01eb677c52bf45
Arjun <arjungup10@gmail.com>
2018-01-22 16:40:58 -0800

    Removed unnecessary imports and comments

--- mycrt-backend/src/capture/test_captureHelper.py
+++ mycrt-backend/src/capture/test_captureHelper.py
@@ -1,20 +1,10 @@
 import unittest
-import os
-import sys
-
 from .mocking.mockBoto import mockBoto
 from .captureHelper import getRDSInstances
 
 class TestCaptureHelper(unittest.TestCase):
-   """
-   Testing for the CaptureHelper
-   """
 
    def test_get_rds_instances_many(self):
-      """
-      The actual test.
-      Any method which starts with ''test_'' will be considered as a test case.
-      """
       testBoto = mockBoto()
       response = getRDSInstances(testBoto)
       expected = ['test1', 'test2']
