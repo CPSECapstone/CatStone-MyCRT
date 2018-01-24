ca1f31efe63e077f5557501542d0131dd82e5fd9
Arjun <arjungup10@gmail.com>
2018-01-24 14:29:35 -0800

    Added new field to mockBoto enabling multiple tests
    Also added some more basic tests to RDS

--- mycrt-backend/src/tests/capture/test_captureHelper.py
+++ mycrt-backend/src/tests/capture/test_captureHelper.py
@@ -7,21 +7,33 @@ from tests.mocking.mockBoto import mockBoto
 from capture.captureHelper import getRDSInstances, getS3Instances
 
 class TestCaptureHelper(unittest.TestCase):
+    def test_get_rds_instances_many(self):
+        testBoto = mockBoto(0)
+        response = getRDSInstances(testBoto)
+        expected = []
 
-   def test_get_rds_instances_many(self):
-      testBoto = mockBoto()
-      response = getRDSInstances(testBoto)
-      expected = ['test1', 'test2']
+        self.assertEqual(response, expected)
 
-      self.assertEqual(response, expected)
+    def test_get_rds_instances_singular(self):
+        testBoto = mockBoto(1)
+        response = getRDSInstances(testBoto)
+        expected = ['test1']
 
-   def test_get_s3_instances_singular(self):
-      testBoto = mockBoto()
-      response = getS3Instances(testBoto)
-      expected = ['testBucket1']
+        self.assertEqual(response, expected)
 
-      self.assertEqual(response, expected)
+    def test_get_rds_instances_many(self):
+        testBoto = mockBoto(2)
+        response = getRDSInstances(testBoto)
+        expected = ['test1', 'test2']
 
+        self.assertEqual(response, expected)
+
+    def test_get_s3_instances_singular(self):
+        testBoto = mockBoto(1)
+        response = getS3Instances(testBoto)
+        expected = ['testBucket1']
+
+        self.assertEqual(response, expected)
 
 if __name__ == '__main__':
    unittest.main()
