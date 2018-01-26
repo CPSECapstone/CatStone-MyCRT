23aca69d9d428e6fc68722707f6644d665aa0196
Arjun <arjungup10@gmail.com>
2018-01-26 15:02:50 -0800

    Added more getS3Instances tests and related classes

--- mycrt-backend/src/tests/mocking/mockBoto.py
+++ mycrt-backend/src/tests/mocking/mockBoto.py
@@ -61,9 +61,20 @@ class mockBoto:
             return exampleResponse
     class mockRDS3:
         def describe_db_instances(self):
-            raise ClientError({'Error': {'Code': '400', 'Message': 'Generic Error'}}, 'describe_db_instances ')
+            raise ClientError({'Error': {'Code': '400', 'Message': 'Generic Error'}}, 'describe_db_instances')
 
-    class mockS3:
+    class mockS30:
+        def list_buckets(self):
+            exampleResponse = {
+            'Owner': {
+                'DisplayName': 'agupta09', 'ID': 'fb58bd8602028b21eb564ccb2743c44481d96f9f999bc480ed1854dd977236f0'},
+                'Buckets': [],
+                'ResponseMetadata': {'RequestId': '173A70D1BF9097D9', 'RetryAttempts': 0,
+                'HTTPHeaders': {'server': 'AmazonS3', 'x-amz-request-id': '173A70D1BF9097D9', 'transfer-encoding': 'chunked', 'content-type': 'application/xml', 'x-amz-id-2': 'wmqbidko+5fBcotqbVhY+mNcCGE/9z+QoU5xMs4Y/tpzI6BX8trQi+72jgkBqavHVQNN61Cpooc=', 'date': 'Tue, 23 Jan 2018 02:52:17 GMT'}, 'HostId': 'wmqbidko+5fBcotqbVhY+mNcCGE/9z+QoU5xMs4Y/tpzI6BX8trQi+72jgkBqavHVQNN61Cpooc=', 'HTTPStatusCode': 200}
+            }
+            return exampleResponse
+
+    class mockS31:
         def list_buckets(self):
             exampleResponse = {
             'Owner': {
@@ -73,6 +84,22 @@ class mockBoto:
                 'HTTPHeaders': {'server': 'AmazonS3', 'x-amz-request-id': '173A70D1BF9097D9', 'transfer-encoding': 'chunked', 'content-type': 'application/xml', 'x-amz-id-2': 'wmqbidko+5fBcotqbVhY+mNcCGE/9z+QoU5xMs4Y/tpzI6BX8trQi+72jgkBqavHVQNN61Cpooc=', 'date': 'Tue, 23 Jan 2018 02:52:17 GMT'}, 'HostId': 'wmqbidko+5fBcotqbVhY+mNcCGE/9z+QoU5xMs4Y/tpzI6BX8trQi+72jgkBqavHVQNN61Cpooc=', 'HTTPStatusCode': 200}
             }
             return exampleResponse
+
+    class mockS32:
+        def list_buckets(self):
+            exampleResponse = {
+            'Owner': {
+                'DisplayName': 'agupta09', 'ID': 'fb58bd8602028b21eb564ccb2743c44481d96f9f999bc480ed1854dd977236f0'},
+                'Buckets': [{'CreationDate': datetime.datetime(2017, 11, 27, 4, 53, tzinfo=tzutc()), 'Name': 'testBucket1'},
+                    {'CreationDate': datetime.datetime(2017, 11, 27, 4, 53, tzinfo=tzutc()), 'Name': 'testBucket2'}],
+                'ResponseMetadata': {'RequestId': '173A70D1BF9097D9', 'RetryAttempts': 0,
+                'HTTPHeaders': {'server': 'AmazonS3', 'x-amz-request-id': '173A70D1BF9097D9', 'transfer-encoding': 'chunked', 'content-type': 'application/xml', 'x-amz-id-2': 'wmqbidko+5fBcotqbVhY+mNcCGE/9z+QoU5xMs4Y/tpzI6BX8trQi+72jgkBqavHVQNN61Cpooc=', 'date': 'Tue, 23 Jan 2018 02:52:17 GMT'}, 'HostId': 'wmqbidko+5fBcotqbVhY+mNcCGE/9z+QoU5xMs4Y/tpzI6BX8trQi+72jgkBqavHVQNN61Cpooc=', 'HTTPStatusCode': 200}
+            }
+            return exampleResponse
+
+    class mockS33:
+        def list_buckets(self):
+            raise ClientError({'Error': {'Code': '400', 'Message': 'Generic Error'}}, 'list_buckets')
 
     def client(self, awsApplication):
         if (awsApplication == 'rds'):
@@ -84,6 +111,12 @@ class mockBoto:
                 return self.mockRDS2()
             elif (self.testNum == 3):
                 return self.mockRDS3()
-            return self.mockRDS0()
         elif (awsApplication == 's3'):
-            return self.mockS3()
+            if (self.testNum == 0):
+                return self.mockS30()
+            elif (self.testNum == 1):
+                return self.mockS31()
+            elif (self.testNum == 2):
+                return self.mockS32()
+            elif (self.testNum == 3):
+                return self.mockS33()
