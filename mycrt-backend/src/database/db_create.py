1f3999eae281b9780bc801255afc3e3dd1bb78b3
Andrew Ly <andyrewly@gmail.com>
2018-01-27 14:45:51 -0800

    Added functions to return True or False if a username or email exists

--- mycrt-backend/src/database/db_create.py
+++ mycrt-backend/src/database/db_create.py
@@ -1,6 +1,14 @@
-from dbConnector import db
-from models import User
+from .dbConnector import db
+from .models import *
 
 db.create_all()
 
 print("DB created")
+
+def createDB():
+    db.create_all()
+    print("DB Created")
+
+def deleteDB():
+    db.drop_all()
+    print("DB Deleted")
