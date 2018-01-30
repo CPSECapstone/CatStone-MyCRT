474d9c7dca34a0d9d247051adf88c7dded71ad7c
Audrey Chan <audreyc@sonic.net>
2018-01-30 10:27:15 -0800

    register dialog

--- mycrt-gui/src/components/Pages/LogIn.js
+++ mycrt-gui/src/components/Pages/LogIn.js
@@ -6,6 +6,8 @@ import '../../bootstrap-3.3.7-dist/css/bootstrap.min.css';
 
 import Button from './Button.js';
 import TextField from 'material-ui/TextField';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 class LogIn extends Component {
   constructor(props) {
@@ -13,12 +15,27 @@ class LogIn extends Component {
     this.state = {
       usernameValue: undefined,
       passwordValue: undefined,
-      isSubmitDisabled: true
+      isSubmitDisabled: true,
+      isRegisterCalloutVisible: false,
+      regUsernameValue: undefined,
+      regPasswordValue: undefined,
+      emailValue: undefined,
+      awsKeyValue: undefined,
+      secretKeyValue: undefined
       };
 
     this.onUsernameChange = this.onUsernameChange.bind(this);
     this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onRegisterClick = this.onRegisterClick.bind(this);
     this.isSubmitDisabled = this.isSubmitDisabled.bind(this);
+    this.onRegisterDismiss = this.onRegisterDismiss.bind(this);
+
+    this.onRegUsernameChange = this.onRegUsernameChange.bind(this);
+    this.onRegPasswordChange = this.onRegPasswordChange.bind(this);
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onAWSKeyChange = this.onAWSKeyChange.bind(this);
+    this.onSecretKeyChange = this.onSecretKeyChange.bind(this);
+    this.isRegisterFieldsFilled = this.isRegisterFieldsFilled.bind(this);
   }
 
   onUsernameChange(event, value) {
@@ -45,11 +62,103 @@ class LogIn extends Component {
     }
   }
 
+  onRegisterClick() {
+    this.setState(prevState => ({
+      isRegisterCalloutVisible: true
+    }));
+  }
+
+  onRegisterDismiss() {
+    this.setState(prevState => ({
+      isRegisterCalloutVisible: false
+    }));
+  }
+
   isSubmitDisabled() {
-    return (this.state.passwordValue != undefined && this.state.usernameValue != undefined)
+    return (this.state.passwordValue != undefined && this.state.usernameValue != undefined);
+  }
+
+  isRegisterFieldsFilled() {
+    return this.state.regUsernameValue != undefined && this.state.regPasswordValue != undefined
+    && this.state.emailValue != undefined && this.state.awsKeyValue != undefined
+    && this.state.secretKeyValue != undefined;
+  }
+
+  onRegUsernameChange(event, value) {
+    if (value.length == 0) {
+      this.setState(prevState => ({
+        regUsernameValue: undefined
+      }));
+    } else {
+      this.setState(prevState => ({
+        regUsernameValue: value
+      }));
+    }
+  }
+
+  onRegPasswordChange(event, value) {
+    if (value.length == 0) {
+      this.setState(prevState => ({
+        regPasswordValue: undefined
+      }));
+    } else {
+      this.setState(prevState => ({
+        regPasswordValue: value
+      }));
+    }
+  }
+
+  onEmailChange(event, value) {
+    if (value.length == 0) {
+      this.setState(prevState => ({
+        emailValue: undefined
+      }));
+    } else {
+      this.setState(prevState => ({
+        emailValue: value
+      }));
+    }
+  }
+
+  onAWSKeyChange(event, value) {
+    if (value.length == 0) {
+      this.setState(prevState => ({
+        awsKeyValue: undefined
+      }));
+    } else {
+      this.setState(prevState => ({
+        awsKeyValue: value
+      }));
+    }
+  }
+
+  onSecretKeyChange(event, value) {
+    if (value.length == 0) {
+      this.setState(prevState => ({
+        secretKeyValue: undefined
+      }));
+    } else {
+      this.setState(prevState => ({
+        secretKeyValue: value
+      }));
+    }
   }
 
   render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.onRegisterDismiss}
+      />,
+      <FlatButton
+        label="Register"
+        primary={true}
+        disabled={!this.isRegisterFieldsFilled()}
+        onClick={this.onRegisterDismiss}
+      />
+    ];
+
     return (
       <div class="LogIn">
         <div class="log-in-content">
@@ -66,7 +175,7 @@ class LogIn extends Component {
           <div class="log-in-item">
             <h5>Password</h5>
              <TextField
-                hintText="Type username here"
+                hintText="Type password here"
                 onChange={this.onPasswordChange}
                 type="password"
               />
@@ -79,11 +188,57 @@ class LogIn extends Component {
             />
           </div>
           <div class="log-in-item">
-            <div class="log-in-register-link">
+            <div class="log-in-register-link" onClick={this.onRegisterClick}>
               <h5>Don't have an account? Click here to register.</h5>
             </div>
           </div>
         </div>
+        <Dialog
+          title="Register Account"
+          actions={actions}
+          modal={true}
+          open={this.state.isRegisterCalloutVisible}
+          autoScrollBodyContent={true}
+        >
+          <div class="register-content">
+            <div class="register-item">
+              Username
+               <TextField
+                  hintText="Type username here"
+                  onChange={this.onRegUsernameChange}
+                />
+            </div>
+            <div class="register-item">
+              Password
+               <TextField
+                  hintText="Type password here"
+                  onChange={this.onRegPasswordChange}
+                  type="password"
+                />
+            </div>
+            <div class="register-item">
+              Email
+               <TextField
+                  hintText="Type email here"
+                  onChange={this.onEmailChange}
+                />
+            </div>
+            <div class="register-item">
+              AWS Key
+               <TextField
+                  hintText="Type aws key here"
+                  onChange={this.onAWSKeyChange}
+                />
+            </div>
+            <div class="register-item">
+              Secret Key
+               <TextField
+                  hintText="Type secret key here"
+                  onChange={this.onSecretKeyChange}
+                />
+            </div>
+          </div>
+        </Dialog>
       </div>
     );
   }
