ec73eb23c8e686047c3f28faebd5f932e34450ee
Natasha <nlcortez@users.noreply.github.com>
2018-02-05 23:34:04 -0800

    Updated method to get all metrics. Datapoints are now sorted by timestamp.

--- mycrt-backend/src/metrics/metrics.py
+++ mycrt-backend/src/metrics/metrics.py
@@ -1,17 +1,74 @@
 import boto3
 import datetime
+from operator import itemgetter
+
 from botocore.exceptions import ClientError
 
-def get_metrics(local_log_file, start_time, end_time, bucket_name, db_identifier):
+def get_all_metrics(local_log_file, start_time, end_time, bucket_name, db_identifier):
     # can remove once attached to capture function because will be passed as parsed datetimes
-    # parsed_start = datetime.strptime(start_time[:-1], "%Y-%m-%dT%H:%M:%S.%f")
-    # parsed_end = datetime.strptime(end_time[:-1], "%Y-%m-%dT%H:%M:%S.%f")
+    start_time = datetime.datetime(2018, 2, 5, 19, 20, 5) - datetime.timedelta(days=1)
+    end_time = datetime.datetime(2018, 2, 5, 19, 20, 5)
+
     local_metric_file = local_log_file + "_metrics"
-    metrics_list = []
+    all_metrics = {"timestamps": [], "CPUUtilization": [], "ReadIOPS": [], "WriteIOPS": [], "FreeableMemory": []}
 
     client = boto3.client('cloudwatch')
     s3 = boto3.client('s3')
 
+    all_metrics["timestamps"] = get_timestamps(client, start_time, end_time, db_identifier)
+    all_metrics["CPUUtilization"] = get_metric_data(client, "CPUUtilization", start_time, end_time, db_identifier)
+    all_metrics["ReadIOPS"] = get_metric_data(client, "ReadIOPS", start_time, end_time, db_identifier)
+    all_metrics["WriteIOPS"] = get_metric_data(client, "WriteIOPS", start_time, end_time, db_identifier)
+    all_metrics["FreeableMemory"] = get_metric_data(client, "FreeableMemory", start_time, end_time, db_identifier)
+
+    for i in range(0, len(all_metrics["timestamps"])):
+        print(all_metrics["timestamps"][i]),
+        print(all_metrics["CPUUtilization"][i]),
+        print(all_metrics["ReadIOPS"][i]),
+        print(all_metrics["WriteIOPS"][i]),
+        print(all_metrics["FreeableMemory"][i])
+
+    # Will need to update how data is saved to the file
+    #with open(local_metric_file, 'w') as f:
+    #        for datapoint in metrics['Datapoints']:
+    #            #f.write(str(datapoint) + '\n')
+                #print(datapoint)
+    #            metrics_list.append(datapoint)
+
+
+
+    #try:
+    #    response = s3.upload_file(local_metric_file, bucket_name, local_metric_file)
+    #except ClientError as e:
+    #    return e
+
+    return all_metrics
+
+def get_metric_data(client, metric_name, start_time, end_time, db_identifier):
+    metrics = client.get_metric_statistics(
+        Namespace='AWS/RDS',
+        MetricName=metric_name,
+        Dimensions=[
+            {
+                'Name': 'DBInstanceIdentifier',
+                'Value': db_identifier
+            },
+        ],
+        StartTime=start_time,
+        EndTime=end_time,
+        Period=60,
+        Statistics=['Average']
+    )
+
+    sorted_metrics = sorted(metrics['Datapoints'], key=itemgetter('Timestamp'))
+    data = []
+
+    for datapoint in sorted_metrics:
+        data.append(datapoint['Average'])
+
+    return data
+
+def get_timestamps(client, start_time, end_time, db_identifier):
     metrics = client.get_metric_statistics(
         Namespace='AWS/RDS',
         MetricName='CPUUtilization',
@@ -21,21 +78,23 @@ def get_metrics(local_log_file, start_time, end_time, bucket_name, db_identifier
                 'Value': db_identifier
             },
         ],
-        StartTime=datetime.datetime.utcnow() - datetime.timedelta(days=1),
-        EndTime=datetime.datetime.utcnow(),
+        StartTime=start_time,
+        EndTime=end_time,
         Period=60,
-        Statistics=['Average'],
-        Unit='Percent'
+        Statistics=['Average']
     )
 
-    with open(local_metric_file, 'w') as f:
-            for datapoint in metrics['Datapoints']:
-                f.write(str(datapoint) + '\n')
-                metrics_list.append(datapoint)
+    sorted_metrics = sorted(metrics['Datapoints'], key=itemgetter('Timestamp'))
+    data = []
 
-    try:
-        response = s3.upload_file(local_metric_file, bucket_name, local_metric_file)
-    except ClientError as e:
-        return e
+    for datapoint in sorted_metrics:
+        data.append(datapoint['Timestamp'])
 
-    return metrics_list
+    return data
+
+
+def main():
+   get_all_metrics("capture001", None, None, "s3-catstone-capture-test", "rds-catstone-metric-test")
+
+if __name__ == '__main__':
+   main()
