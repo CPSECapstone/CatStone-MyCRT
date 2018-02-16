582acdc7377f84c181b0058d5a159e4345baf505
Natasha <nlcortez@users.noreply.github.com>
2018-02-15 16:34:01 -0800

    Updated metrics to include methods for saving and getting metrics.

--- mycrt-backend/src/metrics/metrics.py
+++ mycrt-backend/src/metrics/metrics.py
@@ -4,8 +4,8 @@ from operator import itemgetter
 
 from botocore.exceptions import ClientError
 
-def get_metrics(local_log_file, start_time, end_time, bucket_name, db_identifier, metric_type):
-    local_metric_file = local_log_file + "_metrics"
+def save_metrics(alias, start_time, end_time, bucket_name, db_identifier, metric_type):
+    local_metric_file = alias + ".metrics"
 
     client = boto3.client('cloudwatch')
     s3 = boto3.client('s3')
@@ -30,6 +30,7 @@ def get_metrics(local_log_file, start_time, end_time, bucket_name, db_identifier
 
 
     with open(local_metric_file, "a") as f:
+        f.write(metric_type)
 
         for datapoint in sorted_metrics:
             datapoint_data = {}
@@ -46,5 +47,33 @@ def get_metrics(local_log_file, start_time, end_time, bucket_name, db_identifier
         response = s3.upload_file(local_metric_file, bucket_name, local_metric_file)
     except ClientError as e:
         return e
+
+    return metric_data
+
+def get_metrics(metric_type, metric_alias, bucket_name):
+    s3 = boto3.resource('s3')
+
+    try:
+        s3.Bucket(bucket_name).download_file(metric_alias, metric_alias)
+    except ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
+
+    metric_data = []
+
+    with open(metric_alias, "r") as f:
+        # skips text until metric type is found
+        for line in f:
+            if metric_type in line:
+                break
+
+        # reads text until all metrics are found
+        for line in f:
+            if metric_type not in line:
+                break
+            datapoint = dict(item.split(":") for item in line.split(","))
+            metric_data.append(datapoint)
 
     return metric_data
