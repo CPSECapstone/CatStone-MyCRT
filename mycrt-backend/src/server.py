762cad9843bff481ca7a6d45fb91a7db28b4ab48
Arjun <arjungup10@gmail.com>
2018-02-23 16:39:14 -0800

    Untested Code for getting associated capture/replays from replay

--- mycrt-backend/src/server.py
+++ mycrt-backend/src/server.py
@@ -98,6 +98,27 @@ def post_capture():
         else:
             return jsonify({'error': "Capture failed"}), 400
 
+@app.route('/users/<replayId>/replays', methods=['GET'])
+@auth.login_required
+def get_associated_replays_from_capture(captureId):
+	user_captures = getCaptureFromId(captureId)
+    user_capture = user_captures[0]
+
+	if (len(user_captures) == 0):
+        return str(404)
+    elif (user_capture['userId'] != g.user.get_id()):
+        return str(403)
+
+	user_replays = getReplaysFromCapture(user_capture.captureId)
+
+	if (len(user_replays) == 0):
+		return str(404)
+	for replay in user_replays:
+		if (replay.userId != g.user.get_id())
+			return str(403)
+
+	return jsonify({'count': len(user_replays), 'capture': user_capture, 'userReplays': user_replays})
+
 @app.route('/users/s3Buckets', methods=['GET'])
 @auth.login_required
 def get_s3_instances():
