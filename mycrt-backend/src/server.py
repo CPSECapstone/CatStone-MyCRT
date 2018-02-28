495e5e4a641e7d79c3dc06045736822981ec7481
Arjun <arjungup10@gmail.com>
2018-02-28 12:32:11 -0800

    Added get replay and post replay after merge overwrote them

--- mycrt-backend/src/server.py
+++ mycrt-backend/src/server.py
@@ -95,6 +95,39 @@ def create_app(config={}):
             else:
                 return jsonify({'error': response}), 400
 
+    @app.route('/users/replays', methods=['GET'])
+    @auth.login_required
+    def get_users_replays():
+        current_user = g.user
+
+        #checkAllRDSInstances()
+        userReplays = getUsersReplays(g.userId, db.get_session())
+
+        return jsonify({"count": len(userReplays), "userReplays": userReplays}), 200
+
+    @app.route('/users/replays', methods=['POST'])
+    @auth.login_required
+    def post_replay():
+        if request.headers['Content-Type'] == 'application/json':
+            jsonData = request.json
+
+            if ('capture_id' not in jsonData or
+                'rds_endpoint' not in jsonData or
+                'region_name' not in jsonData or
+                'db_user' not in jsonData or
+                'db_password' not in jsonData or
+                'db_name' not in jsonData or
+                'alias' not in jsonData):
+                    return jsonify({"error": "Missing field in request."}), 400
+
+			#Call replay function here
+	        #response = replay()
+
+            if (isinstance(response, int) and response > -1):
+                return jsonify({'replayId': response}), 201
+            else:
+                return jsonify({'error': "Replay failed"}), 400
+
     @app.route('/users/<replayId>/replays', methods=['GET'])
     @auth.login_required
     def get_associated_replays_from_replay(captureId):
