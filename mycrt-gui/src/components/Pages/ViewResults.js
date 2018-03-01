4e428e746bf4443673d43131dd4b3eeadbfff7a0
andyrewly <andyrewly@gmail.com>
2018-02-28 17:21:18 -0800

    Updated models and somep artial information for view results

--- mycrt-gui/src/components/Pages/ViewResults.js
+++ mycrt-gui/src/components/Pages/ViewResults.js
@@ -46,7 +46,9 @@ class ViewResults extends Component {
       rowNumberSelected: undefined,
       isCompareOpen: false,
       captureDetails: undefined,
+      replayDetails: undefined,
       selectedCaptureRows: [],
+      selectedReplayRows: [],
       isComparisonChartLoaded: true,
       comparisonIndex: -1,
       captures: [],
@@ -62,7 +64,8 @@ class ViewResults extends Component {
       cpuUtilization: [],
       readIOPS:[],
       writeIOPS: [],
-      showCaptureResultsLoading: true
+      showCaptureResultsLoading: true,
+      showReplayResultsLoading: true
     };
 
     // This binding is necessary to make `this` work in the callback
@@ -71,8 +74,10 @@ class ViewResults extends Component {
     this.onCompareClick = this.onCompareClick.bind(this);
     this.onCompareClose = this.onCompareClose.bind(this);
     this.onCaptureRowSelection = this.onCaptureRowSelection.bind(this);
+    this.onReplayRowSelection = this.onReplayRowSelection.bind(this);
 
-    this.getMetricData = this.getMetricData.bind(this);
+    this.getCaptureMetricData = this.getCaptureMetricData.bind(this);
+    this.getReplayMetricData = this.getReplayMetricData.bind(this);
     this.getCaptureData = this.getCaptureData.bind(this);
     this.getReplayData = this.getReplayData.bind(this);
     this.sendData = this.sendData.bind(this);
@@ -81,17 +86,21 @@ class ViewResults extends Component {
     this.renderCaptureTable = this.renderCaptureTable.bind(this);
     this.renderReplayTable = this.renderReplayTable.bind(this);
     this.renderCaptureDetails = this.renderCaptureDetails.bind(this);
+    this.renderReplayDetails = this.renderReplayDetails.bind(this);
     this.renderCompare = this.renderCompare.bind(this);
   }
 
   componentDidMount() {
     var intervalGetAllCaptures = setInterval(this.getCaptureData, 1500);
+    var intervalGetAllReplays = setInterval(this.getReplayData, 1500);
 
-    this.setState({intervalGetAllCaptures: intervalGetAllCaptures});
+    this.setState({intervalGetAllCaptures: intervalGetAllCaptures,
+                   intervalGetAllReplays: intervalGetAllReplays});
   }
 
   componentWillUnmount() {
     clearInterval(this.state.intervalGetAllCaptures);
+    clearInterval(this.state.intervalGetAllReplays);
   }
 
   fillComparisonData(metricName, metric, comparisonArray) {
@@ -119,6 +128,8 @@ class ViewResults extends Component {
       console.log("upper bound is: " + upperBound);
       console.log("current metric length is: " +metric.length);
       for (var l = upperBound - 1; l < metric.length; l++) {
+        console.log("l is currently" + l);
+        console.log(metric[l]);
         newComparisonArray.push(
         {
           'Timestamp': metric[l]['Timestamp']
@@ -140,7 +151,8 @@ class ViewResults extends Component {
       compareWriteIOPS: []
     }));
 
-    this.getMetricData(this.state.selectedCaptureIds[0]);
+    this.getCaptureMetricData(this.state.selectedCaptureIds[0]);
+    this.getReplayMetricData(this.state.selectedReplayIds[0]);
   }
 
   onCompareClick() {
@@ -199,6 +211,48 @@ class ViewResults extends Component {
     }));
   }
 
+  onReplayRowSelection(rows) {
+    console.log(rows);
+
+    // check if compare button should be disabled or enabled
+    var disabled = true;
+    var selectedRows = [];
+
+    if (rows === "all") {
+      for (var i = 0; i < this.state.replays.length; i++) {
+        selectedRows.push(i);
+      }
+    } else if (rows === "none") {
+      for (var i = 0; i < this.state.replays.length; i++) {
+        selectedRows.push(-1);
+      }
+      this.setState(prevState => ({
+        isCompareDisabled: true,
+        selectedReplayRows: selectedRows
+      }));
+      return;
+    } else {
+      selectedRows = rows;
+    }
+
+    if (selectedRows.length > 1 && selectedRows.length <= 3) {
+      disabled = false;
+    }
+    this.setState(prevState => ({
+      isCompareDisabled: disabled,
+      selectedReplayRows: selectedRows
+    }));
+
+    // get capture ids from row indexes
+    var selectedReplayIds = [];
+    for (var i = 0; i < selectedRows.length; i++) {
+      selectedReplayIds.push(this.state.replays[selectedRows[i]].replayId);
+    }
+    this.setState(prevState => ({
+      selectedReplayIds: selectedReplayIds
+    }));
+  }
+
   getCaptureData() {
     var parentContextState = this.props.parentContext.state;
     var component = this;
@@ -222,10 +276,28 @@ class ViewResults extends Component {
   }
 
   getReplayData() {
+    var parentContextState = this.props.parentContext.state;
+    var component = this;
 
+    $.ajax({
+      url: SERVER_PATH + "/users/replays",
+      dataType: 'json',
+      headers: {'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(parentContextState.token + ":")},
+      type: 'GET',
+      success: function(json) {
+        component.setState(prevState => ({
+          replays: json["userReplays"],
+          showReplayResultsLoading: false
+        }));
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   }
   
-  getMetricData(captureId) {
+  getCaptureMetricData(captureId) {
     var parentContextState = this.props.parentContext.state;
     var component = this;
     console.log("getting metric data FOR CAPTURE ID: " + captureId);
@@ -263,7 +335,60 @@ class ViewResults extends Component {
           }));
 
           if ((i + 1) < this.state.selectedCaptureIds.length) {
-            this.getMetricData(this.state.selectedCaptureIds[i + 1]);
+            this.getCaptureMetricData(this.state.selectedCaptureIds[i + 1]);
+          } else {
+              component.setState(prevState => ({
+              isComparisonChartLoaded: true,
+              comparisonIndex: -1
+            }));
+          }
+        }
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+
+  getReplayMetricData(replayId) {
+    var parentContextState = this.props.parentContext.state;
+    var component = this;
+    console.log("getting metric data FOR CAPTURE ID: " + replayId);
+
+    $.ajax({
+      url: SERVER_PATH + "/users/replays/" + replayId + "/metrics",
+      dataType: 'json',
+      headers: {'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(parentContextState.token + ":")},
+      type: 'GET',
+      success: function(json) {
+        component.setState(prevState => ({
+          freeableMemory: json["FreeableMemory"],
+          cpuUtilization: json["CPUUtilization"],
+          readIOPS: json["ReadIOPS"],
+          writeIOPS: json["WriteIOPS"]
+        }));
+
+        var i = this.state.comparisonIndex;
+        console.log("comparison index is: " + i);
+        //combine comparison data
+        if (!this.state.isComparisonChartLoaded && i < this.state.selectedCaptureIds.length + this.state.selectedReplayIds.length) {
+          console.log("gettign comparison data")
+          var fm = this.fillComparisonData('FreeableMemory', json["FreeableMemory"], this.state.compareFreeableMemory);
+          var cu = this.fillComparisonData('CPUUtilization', json["CPUUtilization"], this.state.compareCpuUtilization);
+          var ri = this.fillComparisonData('ReadIOPS', json["ReadIOPS"], this.state.compareReadIOPS);
+          var wi = this.fillComparisonData('WriteIOPS', json["WriteIOPS"], this.state.compareWriteIOPS);
+
+          component.setState(prevState => ({
+            compareFreeableMemory: fm,
+            compareCpuUtilization: cu,
+            compareReadIOPS: ri,
+            compareWriteIOPS: wi,
+            comparisonIndex: i + 1
+          }));
+
+          if ((i + 1) < this.state.selectedReplayIds.length) {
+            this.getReplayMetricData(this.state.selectedReplayIds[i + 1]);
           } else {
               component.setState(prevState => ({
               isComparisonChartLoaded: true,
@@ -284,13 +409,22 @@ class ViewResults extends Component {
     }));
   }
 
-  onOpenDetailsClick(rowIndex, e) {
+  onOpenCaptureDetailsClick(rowIndex, e) {
     this.setState(prevState => ({
       isLogOpen: true,
       captureDetails: this.state.captures[rowIndex]
     }));
 
-    this.getMetricData(this.state.captures[rowIndex].captureId);
+    this.getCaptureMetricData(this.state.captures[rowIndex].captureId);
+  }
+
+  onOpenReplayDetailsClick(rowIndex, e) {
+    this.setState(prevState => ({
+      isLogOpen: true,
+      replayDetails: this.state.replays[rowIndex]
+    }));
+
+    this.getReplayMetricData(this.state.replays[rowIndex].replayId);
   }
 
   sendData(formDataValues) {
@@ -340,7 +474,7 @@ class ViewResults extends Component {
             stripedRows={false}
           >
             {this.state.captures.map( (row, index) => {
-              let boundItemClick = this.onOpenDetailsClick.bind(this, index);
+              let boundItemClick = this.onOpenCaptureDetailsClick.bind(this, index);
               if (row.captureStatus === COMPLETED || row.captureStatus === ERROR) {
                 return(
                 <TableRow key={index} selected={this.state.selectedCaptureRows.indexOf(index) !== -1} >
@@ -383,7 +517,7 @@ class ViewResults extends Component {
           fixedHeader={true}
           selectable={true}
           multiSelectable={true}
-          onRowSelection={this.onCaptureRowSelection}
+          onRowSelection={this.onReplayRowSelection}
         >
           <TableHeader
             displaySelectAll={true}
@@ -405,14 +539,14 @@ class ViewResults extends Component {
             showRowHover={true}
             stripedRows={false}
           >
-            {this.state.captures.map( (row, index) => {
-              let boundItemClick = this.onOpenDetailsClick.bind(this, index);
-              if (row.captureStatus === COMPLETED || row.captureStatus === ERROR) {
+            {this.state.replays.map( (row, index) => {
+              let boundItemClick = this.onOpenReplayDetailsClick.bind(this, index);
+              if (row.replayStatus === COMPLETED || row.replayStatus === ERROR) {
                 return(
-                <TableRow key={index} selected={this.state.selectedCaptureRows.indexOf(index) !== -1} >
-                  <TableRowColumn>{row.captureAlias}</TableRowColumn>
+                <TableRow key={index} selected={this.state.selectedReplayRows.indexOf(index) !== -1} >
+                  <TableRowColumn>{row.replayAlias}</TableRowColumn>
                   <TableRowColumn>
-                    {(row.captureStatus === COMPLETED) ? <div class="result-complete glyphiconstyle glyphicon glyphicon-ok" /> : <div class="result-fail glyphiconstyle glyphicon glyphicon-remove" />}
+                    {(row.replayStatus === COMPLETED) ? <div class="result-complete glyphiconstyle glyphicon glyphicon-ok" /> : <div class="result-fail glyphiconstyle glyphicon glyphicon-remove" />}
                   </TableRowColumn>
                   <TableRowColumn>{row.rdsInstance}</TableRowColumn>
                   <TableRowColumn>{row.startTime}</TableRowColumn>
@@ -424,15 +558,15 @@ class ViewResults extends Component {
               })}
           </TableBody>
         </Table>
-          {this.state.showCaptureResultsLoading &&
+          {this.state.showReplayResultsLoading &&
             <div class="loading-capture-table">
             <span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>
               <h5>Loading...</h5>
             </div>
           }
-          {!this.state.showCaptureResultsLoading && this.state.captures.length === 0 &&
+          {!this.state.showReplayResultsLoading && this.state.replays.length === 0 &&
             <div class="loading-capture-table">
-              <h5>There are no capture results.</h5>
+              <h5>There are no replay results.</h5>
             </div>
           }
       </div>
@@ -456,6 +590,94 @@ class ViewResults extends Component {
     return (
       <Dialog
         title={"Capture: " + this.state.captureDetails.captureAlias}
+        actions={actions}
+        modal={true}
+        autoScrollBodyContent={true}
+        contentStyle={{
+          width: '100%',
+          maxWidth: 'none',
+        }}
+        open={this.state.isLogOpen}
+      >
+        <div>
+          <h4>Status:
+            <div class= {this.state.captureDetails.captureStatus === COMPLETED ? "result-complete" : "result-fail"}>
+            <h5>{this.state.captureDetails.captureStatus === COMPLETED ? "Completed Successfully" : "Terminated With Errors"}</h5>
+            </div>
+          </h4>
+          <h4>RDS Instance:
+            <div>
+            <h5>{this.state.captureDetails.rdsInstance}</h5>
+            </div>
+          </h4>
+          <h4>Start Time:
+            <div>
+            <h5>{this.state.captureDetails.startTime}</h5>
+            </div>
+          </h4>
+          <h4>End Time:
+            <div>
+            <h5>{this.state.captureDetails.endTime}</h5>
+            </div>
+          </h4>
+          <h3>Freeable Memory</h3>
+          <LineChart width={900} height={300} data={this.state.freeableMemory} margin={{top: 5, right: 60, left: 60, bottom: 5}}>
+             <XAxis dataKey="Timestamp"/>
+             <YAxis label={{ value: "Megabytes", angle: -90, position: 'left' }} domain={['dataMin', 'dataMax']}/>
+             <CartesianGrid strokeDasharray="3 3"/>
+             <Tooltip/>
+             <Legend />
+             <Line type="monotone" dataKey="FreeableMemory" stroke="#00bcd4" dot={false} activeDot={{r: 8}}/>
+          </LineChart>
+          <h3>CPU Utilization</h3>
+          <LineChart width={900} height={300} data={this.state.cpuUtilization} margin={{top: 5, right: 60, left: 60, bottom: 5}}>
+             <XAxis dataKey="Timestamp"/>
+             <YAxis label={{ value: "Percentage", angle: -90, position: 'insideLeft' }} domain={[0, 100]}/>
+             <CartesianGrid strokeDasharray="3 3"/>
+             <Tooltip/>
+             <Legend />
+             <Line type="monotone" dataKey="CPUUtilization" stroke="#8884d8" dot={false} activeDot={{r: 8}}/>
+          </LineChart>
+          <h3>Read IOPS</h3>
+          <LineChart width={900} height={300} data={this.state.readIOPS} margin={{top: 5, right: 60, left: 60, bottom: 5}}>
+             <XAxis dataKey="Timestamp"/>
+             <YAxis label={{ value: "Count/Second", angle: -90, position: 'insideLeft' }} domain={['dataMin', 'dataMax']}/>
+             <CartesianGrid strokeDasharray="3 3"/>
+             <Tooltip/>
+             <Legend />
+             <Line type="monotone" dataKey="ReadIOPS" stroke="#00bcd4" dot={false} activeDot={{r: 8}}/>
+          </LineChart>
+          <h3>Write IOPS</h3>
+          <LineChart width={900} height={300} data={this.state.writeIOPS} margin={{top: 5, right: 60, left: 60, bottom: 5}}>
+             <XAxis dataKey="Timestamp"/>
+             <YAxis label={{ value: "Count/Second", angle: -90, position: 'insideLeft' }} domain={['dataMin', 'dataMax']}/>
+             <CartesianGrid strokeDasharray="3 3"/>
+             <Tooltip/>
+             <Legend />
+             <Line type="monotone" dataKey="WriteIOPS" stroke="#8884d8" dot={false} activeDot={{r: 8}}/>
+          </LineChart>
+        </div>
+      </Dialog>
+      );
+  }
+
+  renderReplayDetails() {
+    const actions = [
+      <FlatButton
+        label="Close"
+        primary={true}
+        onClick={this.onLogClose}
+      />,
+      <FlatButton
+        label="Download Log"
+        primary={true}
+        onClick={this.onLogClose}
+      />,
+    ];
+
+    return (
+      <Dialog
+        title={"Replay: " + this.state.replayDetails.replayAlias}
         actions={actions}
         modal={true}
         autoScrollBodyContent={true}
