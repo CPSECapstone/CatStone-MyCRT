62ac517a1184911267439192f5b1643e1167c904
andyrewly <andyrewly@gmail.com>
2018-03-08 08:24:40 -0800

    Added a db function to update a replay's status

--- mycrt-backend/src/database/updateRecords.py
+++ mycrt-backend/src/database/updateRecords.py
@@ -5,9 +5,9 @@ from .models import Capture
 def updateCapture(captureId, captureStatus, db_session):
     """Function to update a given capture's status
 
-       Keyboard arguments:
+       Keyword arguments:
         captureId -- the id of the capture to update
-        captureStatus -- an integer value [1, 0, -1] representing [success, no status, failure]
+        captureStatus -- an integer value [0, 1, 2, 3] representing [not started, in progress, success, error]
     """
 
     try:
@@ -15,3 +15,17 @@ def updateCapture(captureId, captureStatus, db_session):
         db_session.commit()
     except:
 	    db_session.rollback()
+
+def updateReplay(replayId, replayStatus, db_session):
+    """Function to update a given replay's status
+    
+       Keyword arguments:
+         replayId -- the id of the replay to update
+         replayStatus -- an integer value [0, 1, 2, 3] representing [not started, in progress, success, error]
+    """
+
+    try:
+        replay = db_session.query(Replay).filter(Replay.replayId == replayId).update({"replayStatus": replayStatus})
+        db_session.commit()
+    except:
+        db_session.rollback()
