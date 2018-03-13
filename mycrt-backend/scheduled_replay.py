70172f4da07c17d00b573c6d9aa942e144540e58
Natasha <nlcortez@users.noreply.github.com>
2018-03-12 20:38:36 -0700

    Added saving replay metrics.

--- mycrt-backend/scheduled_replay.py
+++ mycrt-backend/scheduled_replay.py
@@ -5,8 +5,9 @@ from src.database.getRecords import getReplayFromId
 from src.metrics.metrics import save_metrics
 import _thread
 
+time_format = "%Y-%m-%d %H:%M:%S"
+
 def main():
-    time_format = "%Y-%m-%d %H:%M:%S"
     try:
         #DATABASE INFO HERE
         connection = pymysql.connect(host='localhost', user="root", port=3306, db="testreplay", connect_timeout=5)
@@ -45,12 +46,20 @@ def check_if_replay_started(connection):
 
 def check_if_replay_completed(connection):
     with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        cursor.execute("SELECT replayId FROM replay WHERE replayStatus = 1 AND replayId NOT IN (SELECT replayId FROM SCHEDULED_QUERY)")
+        cursor.execute("SELECT * FROM replay WHERE replayStatus = 1 AND replayId NOT IN (SELECT replayId FROM SCHEDULED_QUERY)")
         completed_replays = cursor.fetchall()
-        for replayId in completed_replays:
-            cursor.execute("UPDATE replay SET replayStatus = 2 WHERE replayId = $s", (replayId))
+        for replay in completed_replays:
+            end_time = datetime.now().replace(microsecond=0).strftime(time_format)
+            cursor.execute("UPDATE replay SET replayStatus = 2 WHERE replayId = $s", (replay['replayId']))
+            save_replay_metrics(replay, end_time)
         cursor.close()
     connection.commit()
+
+def save_replay_metrics(replay, end_time):
+    save_metrics(replay['replayAlias'], replay['startTime'], end_time, replay['s3Bucket'], replay['rdsInstance'], "CPUUtilization", replay['regionName'])
+    save_metrics(replay['replayAlias'], replay['startTime'], end_time, replay['s3Bucket'], replay['rdsInstance'], "FreeableMemory", replay['regionName'])
+    save_metrics(replay['replayAlias'], replay['startTime'], end_time, replay['s3Bucket'], replay['rdsInstance'], "ReadIOPS", replay['regionName'])
+    save_metrics(replay['replayAlias'], replay['startTime'], end_time, replay['s3Bucket'], replay['rdsInstance'], "WriteIOPS", replay['regionName'])
 
 def run_query(replayId, query):
     try:
