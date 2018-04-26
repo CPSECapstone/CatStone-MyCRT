5865c76a481f0b6c9834af69aad8f313beb5639a
Miguel Duran <mduran0314@gmail.com>
2018-04-26 10:48:39 -0700

    encrypt aws credentials

--- mycrt-backend/main.py
+++ mycrt-backend/main.py
@@ -3,4 +3,4 @@ from src.server import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(port=5000, threaded=True, ssl_context='adhoc')
+    app.run(port=5000, threaded=True)
