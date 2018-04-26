33a62b53ccd7c95e59c80a28221e7d0e3948fa46
Arjun <arjungup10@gmail.com>
2018-04-26 09:31:27 -0700

    Created sendReplayEmail function

--- mycrt-backend/src/email/email_server.py
+++ mycrt-backend/src/email/email_server.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/../..'))
 
 import smtplib
 from email.mime.text import MIMEText
-from src.database.getRecords import getCaptureFromId
+from src.database.getRecords import getCaptureFromId, getReplayFromId
 
 EMAIL = "mycrtNotifications@gmail.com"
 PASS = "myCRTTool"
@@ -40,3 +40,35 @@ def sendCaptureEmail(captureId, userEmail, db_session):
       return 0
    except:
       return 1
+
+def sendReplayEmail(replayId, userEmail, db_session):
+    s = smtplib.SMTP('smtp.gmail.com', port=587)
+
+    userReplay = getReplayFromId(replayId, db_session)[0]
+    print(userReplay['replayStatus']);
+
+    if (userReplay['replayStatus'] == 2):
+       body =  ("Replay '" + userReplay['replayAlias'] + "' on RDS instance '" +
+                 userReplay['rdsInstance'] + "' has succeeded. Please refer to your '" +
+                 userReplay['s3Bucket'] + "' S3 bucket to view the relevant log and metric files")
+       subject = "Success: Replay " + userReplay['replayAlias']
+    elif (userReplay['replayStatus'] == 3):
+       body = ("Replay '" + userReplay['replayAlias'] + "' on RDS instance '" +
+                 userReplay['rdsInstance'] + "' has failed. Please refer to your '" +
+                 userReplay['s3Bucket'] + "' S3 bucket to view the relevant log and metric files")
+       subject = "Failure: Replay " + userReplay['replayAlias']
+
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = EMAIL
+    msg['To'] = userEmail
+
+    try:
+       s.ehlo()
+       s.starttls()
+       s.login(EMAIL, PASS)
+       s.sendmail(EMAIL, userEmail, msg.as_string())
+       s.quit()
+       return 0
+    except:
+       return 1
