a31beb1fdc603dd8636cfb065485010f76f9c805
Audrey Chan <audreyc@sonic.net>
2018-04-29 20:59:40 -0700

    redux template and redirecting pages

--- mycrt-gui/src/App.js
+++ mycrt-gui/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 import Header from './components/Header/Header.js';
 import NavPage from './components/Pages/NavPage.js';
@@ -10,94 +10,28 @@ import LogIn from './components/Pages/LogIn.js';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import $ from 'jquery';
 
-const WINDOW_HREF = window.location.href;
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import * as actionCreators from './actions/actionCreators';
+import Main from './components/Main/Main';
 
-var navLinks = [
-  {
-    name: "Dashboard",
-    href: "dashboard",
-    idx: 0,
-    icon: "glyphicon-dashboard"
-  },
-  {
-    name: "View Results",
-    href: "results",
-    idx: 1,
-    icon: "glyphicon-th-list"
-  }]
-  /*,
-  {
-    name: "Compare",
-    href: "compare",
-    idx: 2,
-    icon: "glyphicon-eye-open"
-  }]*/
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: 0,
-      loggedIn: false,
-      token: undefined,
-    };
-    this.switchTab = this.switchTab.bind(this);
-    this.onLogIn = this.onLogIn.bind(this);
-    this.onLogOut = this.onLogOut.bind(this);
-
-    document.body.style.background = "#333b44";
-  }
-
-  switchTab(idx) {
-    // e.preventDefault();
-    this.setState(prevState => ({
-      selected: idx
-    }));
-    //window.location.hash = navLinks[idx].href;
-  }
-
-  onLogIn(token) {
-    this.setState(prevState => ({
-      loggedIn: true,
-      token: token
-    }));
-
-    document.body.style.background = "#f7f7f7";
-  }
-
-  onLogOut() {
-    this.setState(prevState => ({
-      loggedIn: false,
-      token: undefined
-    }));
-
-    document.body.style.background = "#333b44";
-  }
-
-  render() {
-    console.log("Im in App " + this.state.selected);
-    return (
-      <MuiThemeProvider>
-      <div class="App">
-      {this.state.loggedIn &&
-        <div>
-        <Header onLogOut={this.onLogOut}/>
-        <div class="app-content">
-          <NavBar navLinks={navLinks} switchTab={this.switchTab}/>
-          <NavPage selected={this.state.selected} parentContext={this}/>
-        </div>
-        </div>
-      }
-      {!this.state.loggedIn &&
-        <LogIn
-          onLogIn={this.onLogIn}
-          parentContext={this}
-        />
-      }
-      </div>
-      </MuiThemeProvider>
-    );
-  }
+// These are the properties we'll automatically pass to Main
+function mapStateToProps(state) {
+   console.log("State is " + JSON.stringify(state));
+   //returning reducers
+   return {
+       User: state.User
+   };
 }
+
+function mapDispatchToProps(dispatch) {
+   return bindActionCreators(actionCreators, dispatch);
+}
+
+const App = withRouter(connect(
+   mapStateToProps,
+   mapDispatchToProps
+)(Main));
 
 export default App;
--- mycrt-gui/src/index.js
+++ mycrt-gui/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// import react router
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const router = (
+   <Provider store={store}>
+      <BrowserRouter>
+         <App/>
+      </BrowserRouter>
+   </Provider>
+)
+
+ReactDOM.render(router, document.getElementById('root'));
 registerServiceWorker();
