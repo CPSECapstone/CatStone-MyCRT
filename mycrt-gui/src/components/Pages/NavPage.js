79c1a5e746e6f0b5afd16a51c7b13ccee6706000
Audrey Chan <audreyc@sonic.net>
2018-05-16 17:50:59 -0700

    fix background height and scroll issue

--- mycrt-gui/src/components/Pages/NavPage.js
+++ mycrt-gui/src/components/Pages/NavPage.js
@@ -9,10 +9,6 @@ class NavPage extends Component {
     super(props);
   }
 
-  componentDidUpdate () {
-    window.scrollTo(0, 0)
-  }
-
   render() {
     var parentContext = this.props.parentContext;
     var that = this;
