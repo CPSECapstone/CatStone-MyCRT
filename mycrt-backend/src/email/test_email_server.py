85559b005b9a56dd7fe8944a13a56889a04299b6
Arjun <arjungup10@gmail.com>
2018-05-30 19:59:25 -0700

    Revert "Removed unused test email server file"
    This reverts commit 2ae2136b513a24ec8421a061b9ce4ecb2114db6c.

---
+++ mycrt-backend/src/email/test_email_server.py
@@ -0,0 +1,22 @@
+import unittest
+from .email_server import sendCaptureEmail, sendReplayEmail
+class TestSample(unittest.TestCase):
+   """
+   Our basic test class
+   """
+
+   '''
+   Testing needs to be redone as the email function has changed significantly
+
+   def test_positive_email(self):
+      """
+      The actual test.
+      Any method which starts with ''test_'' will be considered as a test case.
+      """
+      res = email('aly16@calpoly.edu', 'Test Status: Complete', 'Status Complete')
+
+      self.assertEqual(res, 0)
+   '''
+
+if __name__ == '__main__':
+   unittest.main()
